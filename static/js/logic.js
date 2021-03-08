d9148cea784450c527c892df6af8cf84fe2111d1
meghanvl <meghanvluoma@gmail.com>
2021-03-07 18:27:08 -0600

    updates

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,35 +1,86 @@
-const myMap = L.map("mapid", {
-    center: [39.11, -94.62],
-    zoom: 5
-  });
-
-  
-  // Adding tile layer to the map
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
-  }).addTo(myMap);
-
 const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
 
 d3.json(url, function(data) {
+    createFeatures(data.features);
     console.log(data.features);
-    // Creating a geoJSON layer with the retrieved data
-    L.geoJson(data, {
-      style: function(feature) {
-        return {
-          color: "white",
-          fillColor: chooseColor(feature.properties.mag),
-          fillOpacity: 0.5,
-          weight: 1.5
-        };
-      }
+});
+
+function createFeatures(earthquakeData) {
+
+    // Define a function we want to run once for each feature in the features array
+    // Give each feature a popup describing the place and time of the earthquake
+    function onEachFeature(feature, layer) {
+      layer.bindPopup("<h3>" + feature.properties.place +
+        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+    }
+  
+    // Create a GeoJSON layer containing the features array on the earthquakeData object
+    // Run the onEachFeature function once for each piece of data in the array
+    function radiusSize(magnitude) {
+        return magnitude * 2000;
+    }
+
+    function circleColors(magnitude) {
+        if (magnitude < 1) {
+            return "blue"
+        }
+        else if (magnitude < 2) {
+            return "orange"
+        }
+        else if (magnitude < 3) {
+            return "green"
+        }
+        else if (magnitude < 4) {
+            return "red"
+        }
+        else {
+            return "purple"
+        }
+        
+    }
+
+    const earthquake = L.geoJSON(earthquakeData, {
+        pointToLayer: function(earthquakeData, latlng) {
+            return L.circle(latlng, {
+                radius: radiusSize(earthquakeData.properties.mag),
+                color: circleColors(earthquakeData.properties.mag),
+                fillOpacity: 1
+
+            }); 
+        },
+        onEachFeature: onEachFeature
+
+    });
+  
+    // Sending our earthquakes layer to the createMap function
+    createMap(earthquakes);
+  }
+
+function createMap(earthquake) {
+
+    const myMap = L.map("mapid", {
+        center: [37.09, -95.71],
+        zoom: 5
+    });
+    
+    
+    // Adding tile layer to the map
+    L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/streets-v11",
+        accessToken: API_KEY
     }).addTo(myMap);
-  });
+    
+}
+
+const legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function(map) {
+    
+}
 
 // d3.json(url, function(response) {
 //     const markers = L.markerClusterGroup();
