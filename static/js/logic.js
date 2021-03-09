3b750a064fef7c72aa752e13688d61298624865a
meghanvl <meghanvluoma@gmail.com>
2021-03-09 15:07:45 -0600

    updates

--- static/js/logic.js
+++ static/js/logic.js
@@ -20,8 +20,7 @@ d3.json(url, function(data) {
 
 function createFeatures(earthquakeData) {
 
-    // Define a function we want to run once for each feature in the features array
-    // Give each feature a popup describing the place, time, magnitude and depth of the earthquake
+    // function to run once for each feature in the features array with a popup describing the place, time, magnitude and depth of the earthquake
     function onEachFeature(feature, layer) {
       layer.bindPopup("</h3>Place: " + feature.properties.place +
         "</h3><hr>Time: " + new Date(feature.properties.time) + 
@@ -29,7 +28,7 @@ function createFeatures(earthquakeData) {
         "</h3><hr>Depth: " + feature.geometry.coordinates[2]);
     }
   
-
+    // function to set marker size and color
     function marker(feature, location) {
         const mark = {
             stroke: false,
@@ -76,15 +75,7 @@ function createMap(earthquakes) {
 
     });
 
-    // function getColor(d) {
-    //     return d > 90 ? '#ff3333' :
-    //             d > 70  ? '#ff6633' :
-    //             d > 50  ? '#ff9933' :
-    //             d > 30  ? '#ffcc33' :
-    //             d > 10  ? '#ffff33' :
-    //                      '#ccff33';
-    // }
-
+    // create legend for color of markers based on depth of earthquake
     const legend = L.control({position: 'bottomright'});
 
     legend.onAdd = function(map) {
@@ -93,7 +84,7 @@ function createMap(earthquakes) {
         depth = [-10, 10, 30, 50, 70, 90],
         labels = [];
   
-        // loop through our density intervals and generate a label with a colored square for each interval
+        // loop through depth intervals and generate a label with a colored square for each interval
         for (var i = 0; i < depth.length; i++) {
             div.innerHTML +=
                 '<i style="background:' + markerColor(depth[i] + 1) + '">&nbsp&nbsp&nbsp&nbsp</i> ' +
@@ -111,21 +102,3 @@ function createMap(earthquakes) {
   }).addTo(myMap);
 }
     
-
-
-// d3.json(url, function(response) {
-//     const markers = L.markerClusterGroup();
-
-//     for (const i = 0; i < response.length; i++) {
-//         const location = response[i].location;
-//         console.log(location.coordinates[0]);
-
-//         if (location) {
-//             markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
-//                 .bindPopup(response[i].descriptor));
-//         }
-//     }
-
-//     myMap.addLayer(markers);
-// });
-
